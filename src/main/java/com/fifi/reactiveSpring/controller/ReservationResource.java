bcd4b0132722f026920dc47064f3dda1e4360843
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-30 12:14:10 -0400

    hotel is working fine

--- src/main/java/com/fifi/reactiveSpring/controller/ReservationResource.java
+++ src/main/java/com/fifi/reactiveSpring/controller/ReservationResource.java
@@ -19,4 +19,21 @@ public class ReservationResource {
         //reservationService.getReservation(roomId);
         return Mono.just("{}");
     }
+
+    @PostMapping(path = "", produces =  MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation){
+        return Mono.just("{}");
+    }
+
+    @PutMapping(path = "{roomId}", produces =  MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono<String> updatePrice(@PathVariable String roomId,
+            @RequestBody Mono<Reservation> reservation){
+        return Mono.just("{}");
+    }
+    @DeleteMapping(path = "{roomId}")
+    public Mono<Boolean> deleteReservation(@PathVariable String roomId){
+        return Mono.just(true);
+    }
 }
